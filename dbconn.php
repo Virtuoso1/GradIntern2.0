9c1c818cb58eeac31b6974e1469ea8fa15034127
franklinedel <f4247211@gmail.com>
2025-07-10 18:31:23 +0300

    dbconn update

--- dbconn.php
+++ dbconn.php
@@ -2,9 +2,9 @@
 $host = '127.0.0.1:3306';
 $db = 'grad';
 $user = 'root';
-$pass = ''; // update if needed
+$pass = ''; 
 
-$conn = mysqli_connect($host, $user, $password, $database);
+$conn = mysqli_connect($host, $user, $pass, $db);
 
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
